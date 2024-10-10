602d68ed4715b1fa8287e0048345964ecc91d873
Tom Weber <tom-vibrant@gmx.de>
2024-10-10 20:41:27 +0200

    Test setup

--- test/pyswitch/test_scenarios.py
+++ test/pyswitch/test_scenarios.py
@@ -17,12 +17,37 @@ class MockAdafruitMIDI:
         
         def send(self, midi_message):
             pass
+            
+class MockAdafruitMIDIControlChange:    
+    class ControlChange:
+        def __init__(self, control, value):
+            self.control = control
+            self.value = value
+
+class MockAdafruitMIDISystemExclusive:    
+    class SystemExclusive:
+        def __init__(self, manufacturer_id = [0x00, 0x00, 0x00], data = []):
+            self.manufacturer_id = manufacturer_id
+            self.data = data
+
+class MockGC:
+    def collect():
+        pass
+
+    def mem_free():
+        return 0
+
+    def mem_alloc():
+        return 0
 
 #################################################################################################
 
 with patch.dict(sys.modules, {
     "usb_midi": MockUsbMidi(),
-    "adafruit_midi": MockAdafruitMIDI()
+    "adafruit_midi": MockAdafruitMIDI(),
+    "adafruit_midi.control_change": MockAdafruitMIDIControlChange(),
+    "adafruit_midi.system_exclusive": MockAdafruitMIDISystemExclusive(),
+    "gc": MockGC()
 }):
     from lib.pyswitch.controller.Controller import Controller
 
@@ -31,10 +56,13 @@ with patch.dict(sys.modules, {
 class TestSimpleConfig(unittest.TestCase):
     def test_simple(self):
         appl = Controller(
-            ui = None, 
             led_driver = None,
             config = {
 
-            }
+            },
+            value_provider = None,
+            switches = [],
+            displays = [],
+            ui = None            
         )
         
