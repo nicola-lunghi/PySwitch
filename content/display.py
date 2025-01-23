33fbdfb1ce575aecda45b6e9f71da6d876b296f9
Tom Weber <tom-vibrant@gmx.de>
2025-01-23 17:20:30 +0100

    Rig up/down actions

--- content/display.py
+++ content/display.py
@@ -29,7 +29,9 @@ _DISPLAY_HEIGHT = const(240)
 _SLOT_WIDTH = const(120)                 # Slot width on the display
 _SLOT_HEIGHT = const(40)                 # Slot height on the display
 _FOOTER_Y = const(200)
+_RIG_ID_HEIGHT = const(40)
 _RIG_NAME_HEIGHT = const(160)
+_RIG_ID_Y = const(160)
 
 #############################################################################################################################################
 
@@ -53,14 +55,6 @@ DISPLAY_FOOTER_2 = DisplayLabel(
     bounds = DisplayBounds(_SLOT_WIDTH, _FOOTER_Y, _SLOT_WIDTH, _SLOT_HEIGHT)
 )
 
-# Page
-DISPLAY_PAGE = DisplayLabel(
-    layout = {
-        "font": "/fonts/A12.pcf",
-        "backColor": (0, 0, 0)
-    },
-    bounds = DisplayBounds(0, _FOOTER_Y - 20, _DISPLAY_WIDTH, 20)
-)
 
 Splashes = TunerDisplayCallback(
     strobe = True,
@@ -91,11 +85,30 @@ Splashes = TunerDisplayCallback(
                     "text": KemperRigNameCallback.DEFAULT_TEXT,
                 },
 
-                callback = KemperRigNameCallback()
+                callback = KemperRigNameCallback(
+                    show_name = True,
+                    show_rig_id = False
+                )
             ),
 
-            # Pager
-            DISPLAY_PAGE,
+            # Rig ID
+            DisplayLabel(
+                bounds = DisplayBounds(
+                    0,
+                    _RIG_ID_Y,
+                    _DISPLAY_WIDTH,
+                    _RIG_ID_HEIGHT
+                ),
+
+                layout = {
+                    "font": "/fonts/H20.pcf"
+                },
+
+                callback = KemperRigNameCallback(
+                    show_name = False,
+                    show_rig_id = True
+                )
+            ),
 
             # Bidirectional protocol state indicator (dot)
             BidirectionalProtocolState(DisplayBounds(
--- content/inputs.py
+++ content/inputs.py
@@ -4,28 +4,31 @@
 #
 ##############################################################################################################################################
  
-from pyswitch.hardware.Hardware import Hardware
-
-# from pyswitch.misc import Colors
+from pyswitch.hardware.devices.pa_midicaptain_10 import *
 
 from pyswitch.clients.kemper import KemperEffectSlot
-from pyswitch.controller.AnalogAction import AnalogAction
-from display import DISPLAY_FOOTER_1, DISPLAY_FOOTER_2, DISPLAY_HEADER_1, DISPLAY_HEADER_2
+from display import DISPLAY_FOOTER_1, DISPLAY_FOOTER_2, DISPLAY_HEADER_1, DISPLAY_HEADER_2, DISPLAY_HEADER_3
 
-from pyswitch.clients.kemper.actions.rig_select import RIG_SELECT, RIG_SELECT_DISPLAY_TARGET_RIG
-from pyswitch.clients.kemper.actions.bank_select import BANK_SELECT
+from pyswitch.clients.kemper.actions.rig_select import RIG_SELECT_DISPLAY_TARGET_RIG
+from pyswitch.clients.kemper.actions.rig_select_and_morph_state import RIG_SELECT_AND_MORPH_STATE
 from pyswitch.clients.kemper.actions.effect_state import EFFECT_STATE
-from pyswitch.clients.kemper.actions.morph import MORPH_DISPLAY, MORPH_BUTTON
 from pyswitch.clients.kemper.actions.tuner import TUNER_MODE
+from pyswitch.clients.kemper.actions.binary_switch import BINARY_SWITCH
+from pyswitch.clients.kemper.actions.bank_up_down import BANK_UP, BANK_DOWN
 
 from pyswitch.clients.kemper.mappings.rig import MAPPING_RIG_VOLUME
 from pyswitch.clients.kemper.mappings.morph import MAPPING_MORPH_PEDAL
+from pyswitch.clients.kemper.mappings.amp import MAPPING_AMP_GAIN, MAPPING_AMP_STATE
+
+from pyswitch.controller.AnalogAction import AnalogAction
+from pyswitch.controller.EncoderAction import EncoderAction
+
 
 # Defines the switch assignments and other inputs
 Inputs = [
     # Pedal 1
     {
-        "assignment": Hardware.PA_MIDICAPTAIN_10_EXP_PEDAL_1,
+        "assignment": PA_MIDICAPTAIN_10_EXP_PEDAL_1,
         "actions": [
             AnalogAction(
                 mapping = MAPPING_RIG_VOLUME(),
@@ -36,7 +39,7 @@ Inputs = [
 
     # Pedal 2
     {
-        "assignment": Hardware.PA_MIDICAPTAIN_10_EXP_PEDAL_2,
+        "assignment": PA_MIDICAPTAIN_10_EXP_PEDAL_2,
         "actions": [
             AnalogAction(
                 mapping = MAPPING_MORPH_PEDAL(),
@@ -45,94 +48,86 @@ Inputs = [
         ]
     },
 
+    # Wheel rotary encoder
+    {
+        "assignment": PA_MIDICAPTAIN_10_WHEEL_ENCODER,
+        "actions": [
+            EncoderAction(
+                mapping = MAPPING_AMP_GAIN()
+            )
+        ]
+    },
+
+    # Wheel push button
+    {
+        "assignment": PA_MIDICAPTAIN_10_WHEEL_BUTTON,
+        "actions": [
+            BINARY_SWITCH(
+                mapping = MAPPING_AMP_STATE()                
+            )
+        ]
+    },
+
     ####################################################################################
 
     # Switch 1
     {
-        "assignment": Hardware.PA_MIDICAPTAIN_10_SWITCH_1,
+        "assignment": PA_MIDICAPTAIN_10_SWITCH_1,
         "actions": [
-            # EFFECT_STATE(
-            #     slot_id = KemperEffectSlot.EFFECT_SLOT_ID_A,
-            #     display = DISPLAY_HEADER_1
-            # ),
-            MORPH_DISPLAY(
+            EFFECT_STATE(
+                slot_id = KemperEffectSlot.EFFECT_SLOT_ID_A,
                 display = DISPLAY_HEADER_1
-            )
-        ],
-        "actionsHold": [
-            BANK_SELECT(
-                bank = 1,
-                display_mode = RIG_SELECT_DISPLAY_TARGET_RIG
             )
         ]    
     },
 
     # Switch 2
     {
-        "assignment": Hardware.PA_MIDICAPTAIN_10_SWITCH_2,
+        "assignment": PA_MIDICAPTAIN_10_SWITCH_2,
         "actions": [
-            # EFFECT_STATE(
-            #     slot_id = KemperEffectSlot.EFFECT_SLOT_ID_B,
-            #     display = DISPLAY_HEADER_2          
-            # )
-            MORPH_BUTTON(
-                display = DISPLAY_HEADER_2   
-            )
-        ],
-        "actionsHold": [
-            BANK_SELECT(
-                bank = 2,
-                bank_off = 4,
-                display_mode = RIG_SELECT_DISPLAY_TARGET_RIG
+            EFFECT_STATE(
+                slot_id = KemperEffectSlot.EFFECT_SLOT_ID_B,
+                display = DISPLAY_HEADER_2          
             )
         ]    
     },
 
     # Switch 3
     {
-        "assignment": Hardware.PA_MIDICAPTAIN_10_SWITCH_3,
+        "assignment": PA_MIDICAPTAIN_10_SWITCH_3,
         "actions": [
             EFFECT_STATE(
-                slot_id = KemperEffectSlot.EFFECT_SLOT_ID_DLY,
-                display = DISPLAY_FOOTER_1
-            )
-        ],
-        "actionsHold": [
-            BANK_SELECT(
-                bank = 3,
-                display_mode = RIG_SELECT_DISPLAY_TARGET_RIG
+                slot_id = KemperEffectSlot.EFFECT_SLOT_ID_C,
+                display = DISPLAY_HEADER_3
             )
         ]    
     },
     
     # Switch 4
     {
-        "assignment": Hardware.PA_MIDICAPTAIN_10_SWITCH_4,
+        "assignment": PA_MIDICAPTAIN_10_SWITCH_4,
         "actions": [
-                EFFECT_STATE(
-                slot_id = KemperEffectSlot.EFFECT_SLOT_ID_REV,
-                display = DISPLAY_FOOTER_2
+            EFFECT_STATE(
+                slot_id = KemperEffectSlot.EFFECT_SLOT_ID_D,
+                display = DISPLAY_FOOTER_1
             )
         ],
         "actionsHold": [
-            BANK_SELECT(
-                bank = 4,
-                display_mode = RIG_SELECT_DISPLAY_TARGET_RIG
-            )
+            TUNER_MODE()
         ]    
     },
 
     # Switch up
     {
-        "assignment": Hardware.PA_MIDICAPTAIN_10_SWITCH_UP,
+        "assignment": PA_MIDICAPTAIN_10_SWITCH_UP,
         "actions": [
-            TUNER_MODE()
+            EFFECT_STATE(
+                slot_id = KemperEffectSlot.EFFECT_SLOT_ID_X,
+                display = DISPLAY_FOOTER_2
+            )
         ],
         "actionsHold": [
-            BANK_SELECT(
-                bank = 5,
-                display_mode = RIG_SELECT_DISPLAY_TARGET_RIG
-            )
+            BANK_UP()
         ]    
     },
 
@@ -140,96 +135,49 @@ Inputs = [
 
     # Switch A
     {
-        "assignment": Hardware.PA_MIDICAPTAIN_10_SWITCH_A,
-        "actions": [
-            RIG_SELECT(
-                rig = 1,
-                display_mode = RIG_SELECT_DISPLAY_TARGET_RIG,
-                #color = Colors.YELLOW
-            ),
-            MORPH_DISPLAY()
-        ],
-        "actionsHold": [
-            BANK_SELECT(
-                bank = 6,
-                display_mode = RIG_SELECT_DISPLAY_TARGET_RIG
-            )
-        ]    
+        "assignment": PA_MIDICAPTAIN_10_SWITCH_A,
+        "actions": RIG_SELECT_AND_MORPH_STATE(
+            rig = 1,
+            display_mode = RIG_SELECT_DISPLAY_TARGET_RIG
+        )    
     },
 
     # Switch B
     {
-        "assignment": Hardware.PA_MIDICAPTAIN_10_SWITCH_B,
-        "actions": [
-            RIG_SELECT(
-                rig = 2,
-                display_mode = RIG_SELECT_DISPLAY_TARGET_RIG,
-                #color = Colors.BLUE
-            ),
-            MORPH_DISPLAY()
-        ],
-        "actionsHold": [
-            BANK_SELECT(
-                bank = 7,
-                display_mode = RIG_SELECT_DISPLAY_TARGET_RIG
-            )
-        ]    
+        "assignment": PA_MIDICAPTAIN_10_SWITCH_B,
+        "actions": RIG_SELECT_AND_MORPH_STATE(
+            rig = 2,
+            display_mode = RIG_SELECT_DISPLAY_TARGET_RIG
+        )
     },
 
     # Switch C
     {
-        "assignment": Hardware.PA_MIDICAPTAIN_10_SWITCH_C,
-        "actions": [
-            RIG_SELECT(
-                rig = 3,
-                display_mode = RIG_SELECT_DISPLAY_TARGET_RIG,
-                #color = Colors.ORANGE
-            ),
-            MORPH_DISPLAY()
-        ],
-        "actionsHold": [
-            BANK_SELECT(
-                bank = 8,
-                display_mode = RIG_SELECT_DISPLAY_TARGET_RIG
-            )
-        ]    
+        "assignment": PA_MIDICAPTAIN_10_SWITCH_C,
+        "actions": RIG_SELECT_AND_MORPH_STATE(
+            rig = 3,
+            display_mode = RIG_SELECT_DISPLAY_TARGET_RIG
+        )
     },
 
     # Switch D
     {
-        "assignment": Hardware.PA_MIDICAPTAIN_10_SWITCH_D,
-        "actions": [
-            RIG_SELECT(
-                rig = 4,
-                display_mode = RIG_SELECT_DISPLAY_TARGET_RIG,
-                #color = Colors.RED
-            ),
-            MORPH_DISPLAY()
-        ],
-        "actionsHold": [
-            BANK_SELECT(
-                bank = 9,
-                display_mode = RIG_SELECT_DISPLAY_TARGET_RIG
-            )
-        ]    
+        "assignment": PA_MIDICAPTAIN_10_SWITCH_D,
+        "actions": RIG_SELECT_AND_MORPH_STATE(
+            rig = 4,
+            display_mode = RIG_SELECT_DISPLAY_TARGET_RIG
+        )
     },
 
     # Switch down
     {
-        "assignment": Hardware.PA_MIDICAPTAIN_10_SWITCH_DOWN,
-        "actions": [
-            RIG_SELECT(
-                rig = 5,
-                display_mode = RIG_SELECT_DISPLAY_TARGET_RIG,
-                #color = Colors.GREEN
-            ),
-            MORPH_DISPLAY()
-        ],
+        "assignment": PA_MIDICAPTAIN_10_SWITCH_DOWN,
+        "actions": RIG_SELECT_AND_MORPH_STATE(
+            rig = 5,
+            display_mode = RIG_SELECT_DISPLAY_TARGET_RIG
+        ),
         "actionsHold": [
-            BANK_SELECT(
-                bank = 10,
-                display_mode = RIG_SELECT_DISPLAY_TARGET_RIG
-            )
+            BANK_DOWN()
         ]    
     },
 ]
