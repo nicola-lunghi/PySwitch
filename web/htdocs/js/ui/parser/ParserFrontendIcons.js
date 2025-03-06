e922560d73b9f4e7a2b7b71811be17ddd0b5e4d9
Tom Weber <tom-vibrant@gmx.de>
2025-03-06 14:57:19 +0100

    Started action icons

--- web/htdocs/js/ui/parser/ParserFrontendIcons.js
+++ web/htdocs/js/ui/parser/ParserFrontendIcons.js
@@ -13,7 +13,18 @@ class ParserFrontendIcons {
      * Returns icon DOM for the passed action proxy
      */
     get(actionCallProxy) {
+        const icons = [];
+
+        // Page icon
         const page = actionCallProxy.page()
-        console.log(page)
+        if (page) {
+            icons.push(
+                $('<span />')
+                .addClass('icon-page')
+            )
+        }
+        
+
+        return $('<span class="action-icons" />').append(icons)
     }
 }
