ca110b96cb601b0454a1748296c84a000307d9a0
Tom Weber <tom-vibrant@gmx.de>
2025-03-13 14:58:02 +0100

    Pager tests OK

--- test/pyswitch/test_action_pager.py
+++ test/pyswitch/test_action_pager.py
@@ -286,3 +286,364 @@ class TestPagerAction(unittest.TestCase):
         self.assertEqual(pager.label.text, "foo")
         self.assertEqual(pager.label.back_color, (3, 4, 5))
 
+
+    def test_select_page(self):
+        display = DisplayLabel(layout = {
+            "font": "foo",
+            "backColor": (0, 0, 0)
+        })
+
+        pager = PagerAction(
+            pages = [
+                {
+                    "id": 1, 
+                    "color": (3, 4, 5),
+                    "text": "foo"
+                },
+                {
+                    "id": 2, 
+                    "color": (3, 6, 8),
+                    "text": "bar"
+                },
+            ],
+            display = display,
+            led_brightness_on = 0.4,
+            led_brightness_off = 0.3,
+            display_dim_factor_on = 1,
+            display_dim_factor_off = 0.5,
+            select_page = 2
+        )
+
+        action_1 = MockAction({ "enableCallback": pager.enable_callback, "id": 1 })
+        action_2 = MockAction({ "enableCallback": pager.enable_callback, "id": 2 })
+
+        switch = MockFootswitch(
+            actions = [
+                action_1, 
+                action_2
+            ]
+        )
+        
+        switch_pager = MockFootswitch(
+            pixels = [0],
+            actions = [
+                pager
+            ]
+        )
+        
+        appl = MockController2(
+            inputs = [
+                switch, 
+                switch_pager
+            ]
+        )
+        
+        # These calls shall do nothing before init!
+        pager.push()
+        pager.update_displays()
+
+        pager.init(appl, switch_pager)
+        pager.update_displays()
+
+        self.assertEqual(pager.enable_callback.enabled(action_1), True)
+        self.assertEqual(pager.enable_callback.enabled(action_2), False)
+
+        self.assertEqual(switch_pager.color, (3, 6, 8))
+        self.assertEqual(switch_pager.brightness, 0.3)
+
+        self.assertEqual(pager.label.text, "bar")
+        self.assertEqual(pager.label.back_color, (1, 3, 4))
+
+        # Select page 2
+        pager.push()
+
+        self.assertEqual(pager.enable_callback.enabled(action_1), False)
+        self.assertEqual(pager.enable_callback.enabled(action_2), True)
+
+        self.assertEqual(switch_pager.color, (3, 6, 8))
+        self.assertEqual(switch_pager.brightness, 0.4)
+
+        self.assertEqual(pager.label.text, "bar")
+        self.assertEqual(pager.label.back_color, (3, 6, 8))
+
+        # Select page 2 again
+        pager.push()
+
+        self.assertEqual(pager.enable_callback.enabled(action_1), False)
+        self.assertEqual(pager.enable_callback.enabled(action_2), True)
+
+        self.assertEqual(switch_pager.color, (3, 6, 8))
+        self.assertEqual(switch_pager.brightness, 0.4)
+
+        self.assertEqual(pager.label.text, "bar")
+        self.assertEqual(pager.label.back_color, (3, 6, 8))
+
+
+    def test_no_pages(self):
+        pager = PagerAction(
+            pages = []
+        )
+
+        action_1 = MockAction({ "enableCallback": pager.enable_callback, "id": 1 })
+        action_2 = MockAction({ "enableCallback": pager.enable_callback, "id": 2 })
+
+        proxy = pager.proxy(
+            page_id = 10
+        )
+
+        switch = MockFootswitch(
+            actions = [
+                action_1, 
+                action_2
+            ]
+        )
+
+        switch_proxy = MockFootswitch(
+            actions = [
+                proxy
+            ]
+        )
+        
+        switch_pager = MockFootswitch(
+            pixels = [0],
+            actions = [
+                pager
+            ]
+        )
+        
+        appl = MockController2(
+            inputs = [
+                switch, 
+                switch_pager
+            ]
+        )
+        
+        pager.init(appl, switch_pager)
+        proxy.init(appl, switch_proxy)
+        
+        pager.update_displays()
+        proxy.update_displays()
+
+        self.assertEqual(pager.enable_callback.enabled(action_1), False)
+        self.assertEqual(pager.enable_callback.enabled(action_2), False)
+
+        pager.push()
+
+        self.assertEqual(pager.enable_callback.enabled(action_1), False)
+        self.assertEqual(pager.enable_callback.enabled(action_2), False)
+
+        proxy.push()
+
+        self.assertEqual(pager.enable_callback.enabled(action_1), False)
+        self.assertEqual(pager.enable_callback.enabled(action_2), False)
+
+
+    ###########################################################################################
+
+
+    def test_proxy(self):
+        pager = PagerAction(
+            pages = [
+                {
+                    "id": 1, 
+                    "color": (3, 4, 5),
+                    "text": "foo"
+                },
+                {
+                    "id": 2, 
+                    "color": (3, 6, 8),
+                    "text": "bar"
+                },
+            ],
+            led_brightness_on = 0.4,
+            led_brightness_off = 0.3,
+            display_dim_factor_on = 1,
+            display_dim_factor_off = 0.5
+        )
+
+        display_1 = DisplayLabel(layout = {
+            "font": "foo",
+            "backColor": (0, 0, 0)
+        })
+
+        proxy_1 = pager.proxy(
+            page_id = 1,
+            use_leds = True,
+            display = display_1
+        )
+
+        display_2 = DisplayLabel(layout = {
+            "font": "foo",
+            "backColor": (0, 0, 0)
+        })
+
+        proxy_2 = pager.proxy(
+            page_id = 2,
+            use_leds = True,
+            display = display_2
+        )
+
+        action_1 = MockAction({ "enableCallback": pager.enable_callback, "id": 1 })
+        action_2 = MockAction({ "enableCallback": pager.enable_callback, "id": 2 })
+
+        switch = MockFootswitch(
+            actions = [
+                action_1, 
+                action_2
+            ]
+        )
+        
+        switch_pager = MockFootswitch(
+            pixels = [0],
+            actions = [
+                pager
+            ]
+        )
+        
+        switch_proxy_1 = MockFootswitch(
+            pixels = [0],
+            actions = [
+                proxy_1
+            ]
+        )
+
+        switch_proxy_2 = MockFootswitch(
+            pixels = [0],
+            actions = [
+                proxy_2
+            ]
+        )
+
+        appl = MockController2(
+            inputs = [
+                switch, 
+                switch_pager,
+                switch_proxy_1,
+                switch_proxy_2
+            ]
+        )
+        
+        pager.init(appl, switch_pager)
+        proxy_1.init(appl, switch_proxy_1)
+        proxy_2.init(appl, switch_proxy_2)
+
+        pager.update_displays()
+        proxy_1.update_displays()
+        proxy_2.update_displays()
+
+        self.assertEqual(pager.enable_callback.enabled(action_1), True)
+        self.assertEqual(pager.enable_callback.enabled(action_2), False)
+
+        self.assertEqual(switch_proxy_1.color, (3, 4, 5))
+        self.assertEqual(switch_proxy_2.color, (3, 6, 8))
+        self.assertEqual(switch_proxy_1.brightness, 0.4)
+        self.assertEqual(switch_proxy_2.brightness, 0.3)
+
+        self.assertEqual(proxy_1.label.text, "foo")
+        self.assertEqual(proxy_2.label.text, "bar")
+        self.assertEqual(proxy_1.label.back_color, (3, 4, 5))
+        self.assertEqual(proxy_2.label.back_color, (1, 3, 4))
+
+        # Select page 2
+        proxy_2.push()
+
+        self.assertEqual(pager.enable_callback.enabled(action_1), False)
+        self.assertEqual(pager.enable_callback.enabled(action_2), True)
+
+        self.assertEqual(switch_proxy_1.color, (3, 4, 5))
+        self.assertEqual(switch_proxy_2.color, (3, 6, 8))
+        self.assertEqual(switch_proxy_1.brightness, 0.3)
+        self.assertEqual(switch_proxy_2.brightness, 0.4)
+
+        self.assertEqual(proxy_1.label.text, "foo")
+        self.assertEqual(proxy_2.label.text, "bar")
+        self.assertEqual(proxy_1.label.back_color, (1, 2, 2))
+        self.assertEqual(proxy_2.label.back_color, (3, 6, 8))
+
+        # Select page 1
+        proxy_1.push()
+
+        self.assertEqual(pager.enable_callback.enabled(action_1), True)
+        self.assertEqual(pager.enable_callback.enabled(action_2), False)
+
+        self.assertEqual(switch_proxy_1.color, (3, 4, 5))
+        self.assertEqual(switch_proxy_2.color, (3, 6, 8))
+        self.assertEqual(switch_proxy_1.brightness, 0.4)
+        self.assertEqual(switch_proxy_2.brightness, 0.3)
+
+        self.assertEqual(proxy_1.label.text, "foo")
+        self.assertEqual(proxy_2.label.text, "bar")
+        self.assertEqual(proxy_1.label.back_color, (3, 4, 5))
+        self.assertEqual(proxy_2.label.back_color, (1, 3, 4))
+
+    
+    def test_proxy_invalid_page(self):
+        pager = PagerAction(
+            pages = [
+                {
+                    "id": 1, 
+                    "color": (3, 4, 5),
+                    "text": "foo"
+                },
+                {
+                    "id": 2, 
+                    "color": (3, 6, 8),
+                    "text": "bar"
+                },
+            ],
+            led_brightness_on = 0.4,
+            led_brightness_off = 0.3,
+            display_dim_factor_on = 1,
+            display_dim_factor_off = 0.5
+        )
+
+        proxy_1 = pager.proxy(
+            page_id = 30
+        )
+
+        action_1 = MockAction({ "enableCallback": pager.enable_callback, "id": 1 })
+        action_2 = MockAction({ "enableCallback": pager.enable_callback, "id": 2 })
+
+        switch = MockFootswitch(
+            actions = [
+                action_1, 
+                action_2
+            ]
+        )
+        
+        switch_pager = MockFootswitch(
+            pixels = [0],
+            actions = [
+                pager
+            ]
+        )
+        
+        switch_proxy_1 = MockFootswitch(
+            pixels = [0],
+            actions = [
+                proxy_1
+            ]
+        )
+
+        appl = MockController2(
+            inputs = [
+                switch, 
+                switch_pager,
+                switch_proxy_1
+            ]
+        )
+        
+        pager.init(appl, switch_pager)
+        proxy_1.init(appl, switch_proxy_1)
+
+        pager.update_displays()
+        proxy_1.update_displays()
+
+        self.assertEqual(pager.enable_callback.enabled(action_1), True)
+        self.assertEqual(pager.enable_callback.enabled(action_2), False)
+
+        # Select page 2
+        proxy_1.push()
+
+        self.assertEqual(pager.enable_callback.enabled(action_1), True)
+        self.assertEqual(pager.enable_callback.enabled(action_2), False)
