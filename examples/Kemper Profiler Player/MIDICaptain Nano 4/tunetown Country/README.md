bce049310fbd2828c69b412a09c8c03b4e5d12fb
Tom Weber <tom-vibrant@gmx.de>
2025-03-21 15:23:58 +0100

    Added TT Country example

---
+++ examples/Kemper Profiler Player/MIDICaptain Nano 4/tunetown Country/README.md
@@ -0,0 +1,11 @@
+## Exampe Description
+
+This is my personal setting for the Country Rig, which stays for the whole gig in Country settings.
+
+|            | Short Press              | Long Press                                       |  
+|------------|--------------------------|--------------------------------------------------|
+| Switch 1   | Effect State C           |                                                  |
+| Switch 2   | Tap Tempo                | Tuner Mode                                       |
+| Switch A   | Effect State B           |                                                  |
+| Switch B   | Effect State MOD         |                                                  |
+
---
+++ examples/Kemper Profiler Player/MIDICaptain Nano 4/tunetown Country/inputs.py
@@ -0,0 +1,71 @@
+from pyswitch.clients.kemper.actions.tempo import TAP_TEMPO
+from pyswitch.clients.kemper.actions.tempo import SHOW_TEMPO
+from pyswitch.clients.kemper.actions.effect_state import EFFECT_STATE
+from pyswitch.clients.kemper.actions.tuner import TUNER_MODE
+from pyswitch.clients.kemper import KemperEffectSlot
+from display import DISPLAY_HEADER_1
+from display import DISPLAY_HEADER_2
+from display import DISPLAY_FOOTER_1
+from display import DISPLAY_FOOTER_2
+from pyswitch.hardware.devices.pa_midicaptain_nano_4 import *
+
+
+Inputs = [
+    {
+        "assignment": PA_MIDICAPTAIN_NANO_SWITCH_1,
+        "actions": [
+            EFFECT_STATE(
+                slot_id = KemperEffectSlot.EFFECT_SLOT_ID_C, 
+                display = DISPLAY_HEADER_1
+            ),
+            
+        ],
+        "actionsHold": [],
+        
+    },
+    {
+        "assignment": PA_MIDICAPTAIN_NANO_SWITCH_2,
+        "actions": [
+            TAP_TEMPO(
+                use_leds = False
+            ),
+            SHOW_TEMPO(
+                text = 'Tempo'
+            ),
+            
+        ],
+        "actionsHold": [
+            TUNER_MODE(
+                display = DISPLAY_HEADER_2, 
+                text = 'Tuner'
+            ),
+            
+        ],
+        
+    },
+    {
+        "assignment": PA_MIDICAPTAIN_NANO_SWITCH_A,
+        "actions": [
+            EFFECT_STATE(
+                slot_id = KemperEffectSlot.EFFECT_SLOT_ID_B, 
+                display = DISPLAY_FOOTER_1
+            ),
+            
+        ],
+        "actionsHold": [],
+        
+    },
+    {
+        "assignment": PA_MIDICAPTAIN_NANO_SWITCH_B,
+        "actions": [
+            EFFECT_STATE(
+                slot_id = KemperEffectSlot.EFFECT_SLOT_ID_MOD, 
+                display = DISPLAY_FOOTER_2
+            ),
+            
+        ],
+        "actionsHold": [],
+        
+    },
+    
+]
